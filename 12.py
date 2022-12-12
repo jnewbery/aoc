3c729bd9db235d56f388016b74542350b413517a
John Newbery <john@johnnewbery.com>
2022-12-12 12:36:55 +0000

    Add day 12 sol

--- 12.py
+++ 12.py
@@ -1,8 +1,53 @@
+import itertools
+
+def adjacents(coord, grid):
+    if (coord[0] - 1, coord[1]) in grid:
+        yield (coord[0] - 1, coord[1])
+    if (coord[0], coord[1] - 1) in grid:
+        yield (coord[0], coord[1] - 1)
+    if (coord[0] + 1, coord[1]) in grid:
+        yield (coord[0] + 1, coord[1])
+    if (coord[0], coord[1] + 1) in grid:
+        yield (coord[0], coord[1] + 1)
+
+def solve(ll, part):
+    heights = {}
+    distances = [[]]
+
+    for y, l in enumerate(ll):
+        for x, c in enumerate(l):
+            if ord(c) >= ord('a') and ord(c) <= ord('z'):
+                if part == 1 or ord(c) >= ord('b'):
+                    heights[(x, y)] = ord(c) - ord('a')
+                elif part == 2 and ord(c) == ord('a'):
+                    distances[0] += [(x, y, 0)]
+            elif c == 'S':
+                distances[0] += [(x, y, 0)]
+            elif c == 'E':
+                heights[(x, y)] = ord('z') - ord('a')
+                goal = (x, y)
+
+    # print(heights)
+    # print(distances)
+    for d in itertools.count():
+        distances.append([])
+        for coord in distances[d]:
+            for adjacent in adjacents(coord, heights):
+                # print(adjacent)
+                if heights[adjacent] <= coord[2] + 1:
+                    if adjacent[0] == goal[0] and adjacent[1] == goal[1]:
+                        return len(distances) - 1
+
+                    distances[-1].append((adjacent[0], adjacent[1], heights[adjacent]))
+                    del heights[adjacent]
+
+            # print(distances)
+
 def part1(ll):
-    raise NotImplementedError
+    return solve(ll, 1)
 
 def part2(ll):
-    raise NotImplementedError
+    return solve(ll, 2)
 
 TEST_INPUT = """Sabqponm
 abcryxxl
@@ -10,7 +55,7 @@ accszExk
 acctuvwj
 abdefghi"""
 
-TEST_SOL = []
+TEST_SOL = [31, 29]
 
 FULL_INPUT = """abaccccccccccccccaaaccccaaaaaaaaaaaaaccccccaacccccccccccccccccccccccccccccaaaaaa
 abaaccaacccccccccaaaaaccccaaaaaaaaaaaaaccccaaacccccccccccccccccccccccccccccaaaaa
@@ -54,4 +99,4 @@ abaaaaacaaaaccaaaaaaaacaaaaaaaaaaaccccccccccccccccccccaaaccccaaaccccccccccaaacaa
 abaaaaacaaaaccaaaaaaaaaaaaaaaaaaacccccccccccccccccccccccccccccccccccccccccaaaaaa
 abaaacccaaaaccccaaaccccaaaaaaaaaaacccccccccccccccccccccccccccccccccccccccccaaaaa"""
 
-FULL_SOL = []
+FULL_SOL = [391, 386]
