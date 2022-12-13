1afab70892f4e4a00bf27e9f82367e8dc7f07f95
John Newbery <john@johnnewbery.com>
2022-12-13 19:09:21 +0000

    Add 2022 day 11 part 1 solution

--- 2022/11.py
+++ 2022/11.py
@@ -1,5 +1,57 @@
+class Monkey:
+    def __init__(self, items, operation, test_divisor, true_to, false_to):
+        self.items = items
+        self.operation = operation
+        self.test_divisor = test_divisor
+        self.true_to = true_to
+        self.false_to = false_to
+        self.inspections = 0
+        # print(self)
+
+    def __str__(self):
+        return f"{self.items}, {self.operation}, {self.test_divisor}, {self.true_to}, {self.false_to}"
+
+
+def get_monkeys(ll):
+    monkeys = []
+    while ll:
+        ll.pop(0)  # Monkey index
+        items = [int(i) for i in ll.pop(0).split(": ")[1].split(',')]
+        operation = ll.pop(0).split(": new = ")[1]
+        test_divisor = int(ll.pop(0).split(": divisible by ")[1])
+        true_to = int(ll.pop(0).split(": throw to monkey ")[1])
+        false_to = int(ll.pop(0).split(": throw to monkey ")[1])
+        if ll:
+            ll.pop(0)  # blank line
+        monkeys.append(Monkey(items, operation, test_divisor, true_to, false_to))
+
+    return monkeys
+
 def part1(ll):
-    raise NotImplementedError
+    monkeys = get_monkeys(ll)
+    for _ in range(20):
+        for i, monkey in enumerate(monkeys):
+            # print(f"monkey {i}, items: {monkey.items}")
+            while monkey.items:
+                monkey.inspections += 1
+                old = monkey.items.pop(0)  # noqa: F841 (`old` is used in the eval)
+                # print(old)
+                # print(monkey.operation)
+                new = int(eval(monkey.operation))
+                # print(new)
+                new //= 3
+                # print(new)
+                to_monkey = monkey.true_to if (new % monkey.test_divisor) == 0 else monkey.false_to
+                # print(to_monkey)
+                monkeys[to_monkey].items.append(new)
+
+        # for monkey in monkeys:
+        #     print(monkey)
+
+    inspections = sorted([m.inspections for m in monkeys])
+    print(inspections)
+
+    return inspections[-2] * inspections[-1]
 
 def part2(ll):
     raise NotImplementedError
@@ -90,4 +142,4 @@ Monkey 7:
     If true: throw to monkey 6
     If false: throw to monkey 0"""
 
-FULL_SOL = []
+FULL_SOL = [110264]
