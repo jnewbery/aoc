f3c97e26abd03fe8b1476c8c2df63303c697e384
John Newbery <john@johnnewbery.com>
2022-12-13 18:44:44 +0000

    Modernise 2021 solutions

--- run.py
+++ run.py
@@ -33,12 +33,13 @@ def main():
     parser.add_argument("-d", "--day",  type=int, help="Advent of code day. Leave blank to run all days.")
     parser.add_argument("-t", "--test", action="store_true", help="Whether to run with test input. If false, runs with full input.")
     parser.add_argument("-p", "--part", type=int, help="Which part to run. Leave blank to run both parts.")
+    parser.add_argument("-y", "--year", type=int, help="Which year to run.")
 
     args = parser.parse_args()
 
     if not args.day:
         regex = re.compile(r"^\d{2}\.py$")
-        days = sorted([f[0:2] for f in os.listdir() if regex.match(f)])
+        days = sorted([f[0:2] for f in os.listdir(str(args.year)) if regex.match(f)])
     else:
         days = [f"{args.day:02}"]
 
@@ -48,6 +49,7 @@ def main():
         parts = [args.part]
 
     if args.args:
+        print(f"year: {args.year}")
         print(f"days: {days}")
         print(f"test: {args.test}")
         print(f"parts: {parts}")
@@ -55,7 +57,7 @@ def main():
     results = []
     for day in days:
         result = {'Day': f'Day {day}'}
-        mod = importlib.import_module(f"{day}")
+        mod = importlib.import_module(f"{args.year}.{day}")
 
         if args.test:
             ll = mod.TEST_INPUT.splitlines()
