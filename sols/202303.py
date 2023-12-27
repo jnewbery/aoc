d1b02975034b2c032111cd8ca85ce27cc2f20733
John Newbery <john@johnnewbery.com>
2023-12-27 18:28:19 +0000

    202303.03 full solution

--- sols/202303.py
+++ sols/202303.py
@@ -1,58 +1,41 @@
 #!/usr/bin/env python3
 from utils import BaseSolution
 from typing import Optional, Iterable
+import math, re
 
 DIGITS = [str(i) for i in range(10)]
 
-def get_symbols(ll) -> list[set[int]]:
-    symbols: list[set[int]] = [set() for _ in range(len(ll))]
-    for i, line in enumerate(ll):
-        for j, c in enumerate(line):
-            if c not in ["."] + DIGITS:
-                symbols[i].add(j)
+def get_symbols(ll) -> dict[tuple[int, int], list[int]]:
+    """Returns dict of (x,y) -> [] for all symbols in the grid."""
+    board_width = len(ll[0])
+    board_height = len(ll)
+    symbols = {(r, c): [] for r in range(board_width) for c in range(board_height)
+                    if ll[r][c] not in '01234566789.'}
+
+    for r, row in enumerate(ll):
+        # Find every cell boardering a number
+        for n in re.finditer(r'\d+', row):
+            edge = {(r, c) for r in (r-1, r, r+1)
+                           for c in range(n.start()-1, n.end()+1)}
+
+            # For every cell boardering a number, assign that number as a score to the cell
+            # Assumes that no number touches more than one symbol (otherwise it'd be counted twice for part 1)
+            for o in edge & symbols.keys():
+                symbols[o].append(int(n.group()))
+
     return symbols
 
-def get_symbols_for_adjacent_rows(symbols: list[set[int]]) -> list[set[int]]:
-    symbols_adjacent: list[set[int]] = [set() for _ in range(len(symbols))]
-    for i in range(len(symbols)):
-        adjacent_rows = [max(i-1, 0), i, min(i+1, len(symbols)-1)]
-        symbols_adjacent[i] = set.union(*[symbols[j] for j in adjacent_rows])
-
-    return symbols_adjacent
-
-def get_number_locations(line: str) -> Iterable[tuple[int, set[int]]]:
-    """Return a stream of (number, (row, col)) tuples."""
-    # Tracks the current number and its starting location
-    current_number: Optional[tuple[int, set[int]]] = None
-
-    for i, c in enumerate(line):
-        if current_number is None and c in DIGITS:
-            current_number = (int(c), {max(i-1, 0), i})
-        elif current_number is not None and c in DIGITS:
-            current_number = (current_number[0] * 10 + int(c), current_number[1] | {i})
-        elif current_number is not None and c not in DIGITS:
-            yield (current_number[0], current_number[1] | {i})
-            current_number = None
-
-    if current_number is not None:
-        yield (current_number[0], current_number[1])
-
-
 class Solution(BaseSolution):
-    def part1(self, ll):
+    def part1(self, ll: list[str]):
         symbols = get_symbols(ll)
-        symbols_adjacent = get_symbols_for_adjacent_rows(symbols)
 
-        sol = 0
-        for i, l in enumerate(ll):
-            for number, locs in get_number_locations(l):
-                if locs & symbols_adjacent[i]:
-                    sol += number
-
-        return sol
+        return sum(sum(p) for p in symbols.values())
 
     def part2(self, ll):
-        self.exit_not_implemented()
+        symbols = get_symbols(ll)
+
+        # Assumes that no other symbol (eg & or #) touches two numbers
+        return sum(math.prod(p) for p in symbols.values() if len(p)==2)
 
 if __name__ == "__main__":
     Solution()
