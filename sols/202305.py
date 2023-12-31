417165426ce0603086b6dc7c6da59b8a19f4e86c
John Newbery <john@johnnewbery.com>
2023-12-31 16:52:01 +0000

    Add 202305 inputs and template

---
+++ sols/202305.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from utils import BaseSolution
+
+class Solution(BaseSolution):
+    def part1(self, ll):
+        raise NotImplementedError
+
+    def part2(self, ll):
+        raise NotImplementedError
+
+if __name__ == "__main__":
+    Solution()
