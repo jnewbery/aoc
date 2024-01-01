057e95c769b3ba5a7785bd158d25efc7771235d8
John Newbery <john@johnnewbery.com>
2024-01-01 22:37:33 +0000

    Partial: solution for 202305

--- sols/202305.py
+++ sols/202305.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
-from utils import BaseSolution
+from typing import Iterable
+from utils import BaseSolution, get_numbers
+
+class
+
+def get_map_strings(ll: list[str]) -> Iterable[list[str]]:
+    ret: list[str] = []
+    for l in ll:
+        if "map" in l:
+            ret = []
+        elif len(l) == 0:
+            yield ret
+        else:
+            ret.append(l)
+    yield ret
+
 
 class Solution(BaseSolution):
     def part1(self, ll):
-        raise NotImplementedError
+        seeds = get_numbers(ll[0])
+        print(seeds)
+        map_strings = list(get_map_strings(ll[2:]))
+        print(map_strings)
 
     def part2(self, ll):
         raise NotImplementedError
