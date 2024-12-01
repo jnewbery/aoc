19020a0e8b54327b380fbf15d33cb488a26b2ed8
John Newbery <john@johnnewbery.com>
2024-12-01 21:51:34 +0000

    202401: misc

--- src/bin/202401a.rs
+++ src/bin/202401a.rs
@@ -2,24 +2,24 @@ static _TEST_INPUT: &str = include_str!("202401_test_input.txt");
 static _INPUT: &str = include_str!("202401_input.txt");
 
 fn main() {
-    // Parse lines into columns
-    let mut columns: Vec<Vec<i32>> = vec![vec![], vec![]];
+    // Parse lines into lists
+    let mut lists: Vec<Vec<i32>> = vec![vec![], vec![]];
     _INPUT.lines().for_each(|line| {
         let parts: Vec<i32> = line
             .split_whitespace()
             .map(|s| s.parse::<i32>().expect("Failed to parse column"))
             .collect();
-        columns[0].push(parts[0]);
-        columns[1].push(parts[1]);
+        lists[0].push(parts[0]);
+        lists[1].push(parts[1]);
     });
 
-    // Sort the columns
-    columns.iter_mut().for_each(|inner_vec| inner_vec.sort());
+    // Sort the lists
+    lists.iter_mut().for_each(|inner_vec| inner_vec.sort());
 
     // Calculate the sum of absolute differences
-    let sum: i32 = columns[0]
+    let sum: i32 = lists[0]
         .iter()
-        .zip(columns[1].iter())
+        .zip(lists[1].iter())
         .map(|(&a, &b)| (a - b).abs())
         .sum();
 
--- src/bin/202401b.rs
+++ src/bin/202401b.rs
@@ -4,17 +4,18 @@ static _TEST_INPUT: &str = include_str!("202401_test_input.txt");
 static _INPUT: &str = include_str!("202401_input.txt");
 
 fn main() {
-    // Parse the input into two hashmaps
+    // Parse lines into hashmaps of counts
     let (counts1, counts2) = _INPUT.lines().fold((HashMap::new(), HashMap::new()), |(mut acc1, mut acc2), line| {
-        let parts: Vec<&str> = line.split_whitespace().collect();
-        let a = parts[0].parse::<i32>().expect("Failed to parse first column");
-        let b = parts[1].parse::<i32>().expect("Failed to parse second column");
-        *acc1.entry(a).or_insert(0) += 1;
-        *acc2.entry(b).or_insert(0) += 1;
+        let parts: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().expect("Failed to parse column"))
+            .collect();
+        *acc1.entry(parts[0]).or_insert(0) += 1;
+        *acc2.entry(parts[1]).or_insert(0) += 1;
         (acc1, acc2)
     });
 
-    // Calculate the product of the counts of the two columns
+    // Calculate the product of the counts for each entry
     let result: i32 = counts1.iter()
         .filter_map(|(key, value1)| counts2.get(key).map(|value2| key * value1 * value2))
         .sum();
