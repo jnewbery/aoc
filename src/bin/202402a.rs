2076eff1674d00d1e86521fb37ef336992305840
John Newbery <john@johnnewbery.com>
2024-12-02 13:23:32 +0000

    First attempt at 202402a

--- src/bin/202402a.rs
+++ src/bin/202402a.rs
@@ -2,6 +2,7 @@ static _TEST_INPUT: &str = include_str!("202402_test_input.txt");
 static _INPUT: &str = include_str!("202402_input.txt");
 
 fn is_safe(vec: Vec<i32>) -> bool {
+    // println!("Checking vec: {:?}", vec);
     vec.windows(2)
         .map(|pair| pair[1] - pair[0]) // Compute the differences
         .fold(Ok(None), |is_inc, diff| match is_inc {
@@ -10,11 +11,11 @@ fn is_safe(vec: Vec<i32>) -> bool {
             // - OK(Some(true)) is increasing
             // - OK(Some(false)) is decreasing
             // - Err(()) is not monotonic
-            Ok(None) if [1,2].contains(&diff) => Ok(Some(true)), // Initialize to increasing
-            Ok(None) if [-1,-2].contains(&diff) => Ok(Some(false)), // Initialize to decreasing
+            Ok(None) if [1,2,3].contains(&diff) => Ok(Some(true)), // Initialize to increasing
+            Ok(None) if [-1,-2,-3].contains(&diff) => Ok(Some(false)), // Initialize to decreasing
 
-            Ok(Some(true)) if [1,2].contains(&diff) => Ok(Some(true)), // Continue increasing
-            Ok(Some(false)) if [-1,-2].contains(&diff) => Ok(Some(false)), // Continue decreasing
+            Ok(Some(true)) if [1,2,3].contains(&diff) => Ok(Some(true)), // Continue increasing
+            Ok(Some(false)) if [-1,-2,-3].contains(&diff) => Ok(Some(false)), // Continue decreasing
 
             _ => Err(()),
         })
@@ -23,13 +24,13 @@ fn is_safe(vec: Vec<i32>) -> bool {
 
 fn main() {
     let mut ret = 0;
-    for line in _TEST_INPUT.lines() {
+    for line in _INPUT.lines() {
         let vec: Vec<i32> = line.split_whitespace().map(|s| s.parse::<i32>().expect("Failed to parse column")).collect();
-        if is_safe(vec.clone()) {
-            println!("safe: {:?}", vec);
+        if is_safe(vec) {
+            // println!("safe!");
             ret += 1;
         } else {
-            println!("unsafe: {:?}", vec);
+            // println!("unsafe!");
         }
 
     }
