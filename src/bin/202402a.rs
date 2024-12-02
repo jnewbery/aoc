6f4089f35e8ef89ce54ab0e535793bf977aec7ed
John Newbery <john@johnnewbery.com>
2024-12-02 13:17:56 +0000

    First attempt at 202402a

---
+++ src/bin/202402a.rs
@@ -0,0 +1,38 @@
+static _TEST_INPUT: &str = include_str!("202402_test_input.txt");
+static _INPUT: &str = include_str!("202402_input.txt");
+
+fn is_safe(vec: Vec<i32>) -> bool {
+    vec.windows(2)
+        .map(|pair| pair[1] - pair[0]) // Compute the differences
+        .fold(Ok(None), |is_inc, diff| match is_inc {
+            // is_inc indicates whether the sequence is increasing or decreasing
+            // - OK(None) is uninitialized
+            // - OK(Some(true)) is increasing
+            // - OK(Some(false)) is decreasing
+            // - Err(()) is not monotonic
+            Ok(None) if [1,2].contains(&diff) => Ok(Some(true)), // Initialize to increasing
+            Ok(None) if [-1,-2].contains(&diff) => Ok(Some(false)), // Initialize to decreasing
+
+            Ok(Some(true)) if [1,2].contains(&diff) => Ok(Some(true)), // Continue increasing
+            Ok(Some(false)) if [-1,-2].contains(&diff) => Ok(Some(false)), // Continue decreasing
+
+            _ => Err(()),
+        })
+        .is_ok()
+}
+
+fn main() {
+    let mut ret = 0;
+    for line in _TEST_INPUT.lines() {
+        let vec: Vec<i32> = line.split_whitespace().map(|s| s.parse::<i32>().expect("Failed to parse column")).collect();
+        if is_safe(vec.clone()) {
+            println!("safe: {:?}", vec);
+            ret += 1;
+        } else {
+            println!("unsafe: {:?}", vec);
+        }
+
+    }
+
+    println!("ret: {:?}", ret);
+}
