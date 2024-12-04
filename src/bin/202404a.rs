cb369a025afa2ccfa6016cbd13e45e2b228c038b
John Newbery <john@johnnewbery.com>
2024-12-04 18:34:32 +0000

    Fix 202404a
    I didn't realise that the XMAS needed to be in a straight line

--- src/bin/202404a.rs
+++ src/bin/202404a.rs
@@ -3,17 +3,44 @@ use itertools::iproduct;
 static _TEST_INPUT: &str = include_str!("202404_test_input.txt");
 static _INPUT: &str = include_str!("202404_input.txt");
 
-fn dfs(grid: &Vec<Vec<char>>, x: usize, y: usize) {
-    let value = grid.get(y).unwrap().get(x).unwrap();
-    println!("Character at ({}, {}): {}", x, y, value);
+fn get_directions() -> Vec<(i32, i32)> {
+    iproduct!(-1..=1, -1..=1)
+        .filter(|&(x, y)| !(x == 0 && y == 0))
+        .map(|(x, y)| (x, y))
+        .collect()
+}
+
+fn dfs(grid: &Vec<Vec<char>>, x: usize, y: usize, target: &str, dir: &(i32, i32)) -> i32 {
+    let value = match grid.get(y).and_then(|row| row.get(x)) {
+        Some(&c) => c,
+        None => {
+            // Out of bounds!
+            return 0;
+        }
+    };
+
+    if value != target.chars().next().unwrap() {
+        // cell content does not match target
+        return 0;
+    }
+
+    if target.len() == 1 {
+        // target fully matched
+        return 1
+    }
+
+    dfs(grid, (x as i32 + dir.0) as usize, (y as i32 + dir.1) as usize, &target[1..], dir)
 }
 
 fn main() {
-    let grid: Vec<Vec<char>> = _INPUT.lines()
+    let target = "XMAS";
+    let grid: Vec<Vec<char>> = _TEST_INPUT.lines()
         .map(|line| line.chars().collect())
         .collect();
 
-    for (y, x) in iproduct!(0..grid.len(), 0..grid[0].len()) {
-        dfs(&grid, x, y);
-    }
+    let sum: i32 = iproduct!(0..grid.len(), 0..grid[0].len(), get_directions())
+        .map(|(y, x, dir)| dfs(&grid, x, y, &target, &dir))
+        .sum();
+
+    println!("Total sum: {}", sum);
 }
