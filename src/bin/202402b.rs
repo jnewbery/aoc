25d889bc31dbb0d4ebabc223891f89d2e860f0e9
John Newbery <john@johnnewbery.com>
2024-12-04 22:14:21 +0000

    Improve 202402b

--- src/bin/202402b.rs
+++ src/bin/202402b.rs
@@ -4,17 +4,17 @@ static _INPUT: &str = include_str!("202402_input.txt");
 fn is_safe(vec: &[i32]) -> bool {
     vec.windows(2)
         .map(|pair| pair[1] - pair[0]) // Compute the differences
-        .fold(Ok(None), |is_inc, diff| match is_inc {
+        .try_fold(None, |is_inc, diff| match is_inc {
             // is_inc indicates whether the sequence is increasing or decreasing
             // - OK(None) is uninitialized
             // - OK(Some(true)) is increasing
             // - OK(Some(false)) is decreasing
             // - Err(()) is not monotonic
-            Ok(None) if [1,2,3].contains(&diff) => Ok(Some(true)), // Initialize to increasing
-            Ok(None) if [-1,-2,-3].contains(&diff) => Ok(Some(false)), // Initialize to decreasing
+            None if [1, 2, 3].contains(&diff) => Ok(Some(true)), // Initialize to increasing
+            None if [-1, -2, -3].contains(&diff) => Ok(Some(false)), // Initialize to decreasing
 
-            Ok(Some(true)) if [1,2,3].contains(&diff) => Ok(Some(true)), // Continue increasing
-            Ok(Some(false)) if [-1,-2,-3].contains(&diff) => Ok(Some(false)), // Continue decreasing
+            Some(true) if [1, 2, 3].contains(&diff) => Ok(Some(true)), // Continue increasing
+            Some(false) if [-1, -2, -3].contains(&diff) => Ok(Some(false)), // Continue decreasing
 
             _ => Err(()),
         })
@@ -23,20 +23,25 @@ fn is_safe(vec: &[i32]) -> bool {
 
 fn missing_element_iterator(vec: Vec<i32>) -> impl Iterator<Item = Vec<i32>> {
     (0..vec.len()).map(move |i| {
-        let mut new_vec = vec.clone();
-        new_vec.remove(i);
+        let mut new_vec = Vec::with_capacity(vec.len() - 1);
+        new_vec.extend_from_slice(&vec[..i]);
+        new_vec.extend_from_slice(&vec[i + 1..]);
         new_vec
     })
 }
 
 fn main() {
-    let mut sol = 0;
-    for line in _INPUT.lines() {
-        let vec: Vec<i32> = line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
-        if missing_element_iterator(vec).any(|vec| is_safe(&vec)) {
-            sol += 1;
-        }
-    }
+    let sol = _INPUT
+        .lines()
+        .map(|line| {
+            let vec: Vec<i32> = line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+            if missing_element_iterator(vec).any(|vec| is_safe(&vec)) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum::<i32>();
 
     println!("sol: {:?}", sol);
 }
