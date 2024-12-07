f38e2d431b1e8db06b3f66eda6b0f1a11ddf340f
John Newbery <john@johnnewbery.com>
2024-12-07 16:53:10 +0000

    Add soln for 202407

---
+++ src/bin/202407_2.rs
@@ -0,0 +1,43 @@
+use std::collections::HashSet;
+
+static _TEST_INPUT: &str = include_str!("202407_test_input.txt");
+static _INPUT: &str = include_str!("202407_input.txt");
+
+fn test_line(line: &str) -> i64 {
+    let mut operands = line.split(|c: char| !c.is_numeric() && c != '-')
+        .filter_map(|s| s.parse::<i64>().ok());
+    let target = operands.next().unwrap();
+
+    let mut results: HashSet<i64> = HashSet::new();
+    results.insert(operands.next().unwrap());
+
+    for operand in operands {
+        let mut new_results = HashSet::new();
+        for result in results.iter() {
+            if result + operand <= target {
+                new_results.insert(result + operand);
+            }
+            if result * operand <= target {
+                new_results.insert(result * operand);
+            }
+
+            let con: i64 = format!("{}{}", result.to_string(), operand.to_string()).parse().unwrap();
+            if con <= target {
+                new_results.insert(con);
+            }
+
+        }
+        results = new_results;
+    }
+
+    if results.contains(&target) {
+        return target;
+    }
+
+    0
+}
+
+fn main() {
+    let sol = _INPUT.lines().map(|line| test_line(line)).sum::<i64>();
+    println!("{:?}", sol);
+}
