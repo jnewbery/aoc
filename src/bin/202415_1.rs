4bf1a62ebb52b2911780880d8f3dc8dd7ddaa505
John Newbery <john@johnnewbery.com>
2024-12-15 11:28:29 +0000

    Add skeleton and inputs for 202415_1

---
+++ src/bin/202415_1.rs
@@ -0,0 +1,17 @@
+static TEST: bool = true;
+
+static _TEST_INPUT: &str = include_str!("inputs/202415_test.txt");
+static _INPUT: &str = include_str!("inputs/202415.txt");
+
+const INPUT: &str = if TEST { _TEST_INPUT } else { _INPUT };
+
+type Point = (i32, i32);
+enum cell {
+    Empty,
+    Wall,
+    Box,
+}
+
+fn main() {
+    println!("{}", INPUT);
+}
