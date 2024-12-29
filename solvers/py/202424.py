2e928d206b81c64a8554c131a06a11d72e46ca3e
John Newbery <john@johnnewbery.com>
2024-12-29 20:58:24 +0000

    Fixup 202424 py solver

--- solvers/py/202424.py
+++ solvers/py/202424.py
@@ -8,7 +8,6 @@ class Gate:
     output: str
     operation: str
     resolved: int | None = None
-    true_output: str | None = None
 
 def resolve_wire(wire_name: str, wires: dict[str, Gate]) -> int:
     wire = wires[wire_name]
@@ -27,43 +26,36 @@ def resolve_wire(wire_name: str, wires: dict[str, Gate]) -> int:
         input2_val = resolve_wire(input2, wires)
 
     output = 0
-    if wire.output == "AND":
+    if wire.operation == "AND":
         output = input1_val & input2_val
-    elif wire.output == "OR":
+    elif wire.operation == "OR":
         output = input1_val | input2_val
-    elif wire.output == "XOR":
+    elif wire.operation == "XOR":
         output = input1_val ^ input2_val
 
     wire.resolved = output
     return output
 
-def get_n_bits(n: int, wires: list[Gate], prefix: str = "") -> str:
-    values = "".join([str(w.resolved) for w in wires if w.output.startswith(prefix) and int(w.output[1:]) <= n])
-    return values
-
-def get_x_plus_y_n_bits(wires: dict[str, Gate], n: int) -> int:
-    xy_wires = [w for w in wires.values() if w.output.startswith('x') or w.output.startswith('y')]
-    xy_wires.sort(key=lambda w: w.output, reverse=True)
-    x = get_n_bits(n, xy_wires, 'x')
-    # print(f"x = {x}")
-    y = get_n_bits(n, xy_wires, 'y')
-    # print(f"y = {y}")
-    # print(f"x + y = {int(x, 2) + int(y, 2)}")
-    return int(x, 2) + int(y, 2) & (2**(n+1)-1)
-
-def solve_to_n(wires: dict[str, Gate], n: int) -> str:
-    for wire_name in [w for w in wires if (w.startswith("z")) and int(w[1:]) <= n]:
-        resolve_wire(wire_name, wires)
-
-    resolved_wires = [w for w in wires.values() if w.resolved is not None]
-    resolved_wires.sort(key=lambda w: w.output, reverse=True)
-
-    return get_n_bits(n, resolved_wires, 'z')
-
 class Solution(BaseSolution):
     def part1(self, ll) -> str:
-        del ll
-        raise NotImplementedError()
+        wires: dict[str, Gate] = {}
+        for l in ll:
+            if ":" in l:
+                wire, value = l.split(": ")
+                wires[wire.strip()] = Gate(inputs=("", ""), output=wire.strip(), operation="", resolved=int(value.strip()))
+            elif "->" in l:
+                parts = l.split(" ")
+                operation = parts[1]
+                gate = Gate(inputs=(parts[0], parts[2]), output=parts[4], operation=operation)
+                wires[gate.output] = gate
+
+        for wire_name in wires:
+            resolve_wire(wire_name, wires)
+
+        wires_list = list(wires.values())
+        wires_list.sort(key=lambda x: x.output, reverse=True)
+        z_values = "".join([str(wire.resolved) for wire in wires_list if wire.output[0] == "z"])
+        return(str(int(z_values, 2)))
 
     def part2(self, ll) -> str:
         gates: list[Gate] = []
@@ -74,20 +66,7 @@ class Solution(BaseSolution):
                 assert operation is not None
                 gates.append(Gate(inputs=(parts[0], parts[2]), output=parts[4], operation=operation))
 
-        # good_wires: dict[str, Gate] = {k: v for k, v in wires.items() if k.startswith("x") or k.startswith("y")}
-        # good_wires['z00'] = Gate(inputs=('x00', 'y00'), output='z00', operation=(lambda a, b: a ^ b)) # z bit
-        # good_wires['c00'] = Gate(inputs=('x00', 'y00'), output='c00', operation=(lambda a, b: a & b)) # carry
-        # for i in range(1, 45):
-        #     good_wires[f'o{i:02}'] = Gate(inputs=(f'x{i:02}', f'y{i:02}'), output=f'o{i:02}', operation=(lambda a, b: a ^ b))  # or
-        #     good_wires[f'z{i:02}'] = Gate(inputs=(f'o{i:02}', f'c{i-1:02}'), output=f'z{i:02}', operation=(lambda a, b: a ^ b))  # z bit
-        #     good_wires[f'a{i:02}'] = Gate(inputs=(f'x{i:02}', f'y{i:02}'), output=f'a{i:02}', operation=(lambda a, b: a & b))  # and
-        #     good_wires[f'pc{i:02}'] = Gate(inputs=(f'o{i:02}', f'c{i-1:02}'), output=f'pc{i:02}', operation=(lambda a, b: a & b))  # partial carry
-        #     if i < 44:
-        #         good_wires[f'c{i:02}'] = Gate(inputs=(f'a{i:02}', f'pc{i:02}'), output=f'c{i:02}', operation=(lambda a, b: a | b))  # carry
-        #     else:
-        #         good_wires['z45'] = Gate(inputs=(f'a{i:02}', f'pc{i:02}'), output='z45', operation=(lambda a, b: a | b))  # carry
         bad_gates: set[str] = set()
-        print(len(gates))
         for gate in gates:
             if gate.output[0] == 'z' and not (gate.output == 'z45' or gate.operation == 'XOR'):
                 bad_gates.add(gate.output)
