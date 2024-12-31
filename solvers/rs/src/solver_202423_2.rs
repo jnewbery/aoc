76149acdc1085b6337b3533745dd3d77340f69da
John Newbery <john@johnnewbery.com>
2024-12-31 14:10:53 +0000

    Implement Bron-Kerbosch for 202423_2

--- solvers/rs/src/solver_202423_2.rs
+++ solvers/rs/src/solver_202423_2.rs
@@ -1,48 +1,56 @@
 use fxhash::{FxHashMap, FxHashSet};
+use itertools::Itertools;
 
-fn expand_subsets(subsets: &FxHashSet<Vec<String>>, pairs: &FxHashMap<String, Vec<String>>) -> FxHashSet<Vec<String>> {
-    let mut new_subsets = FxHashSet::default();
-    for subset in subsets.iter() {
-        for next in pairs.get(&subset[0]).unwrap() {
-            if subset.contains(next) {
-                continue;
-            }
-            if subset.into_iter().map(|s| pairs.get(s).unwrap()).all(|p| p.contains(next)) {
-                let mut new_subset = subset.clone();
-                new_subset.insert(0, next.clone());
-                new_subset.sort();
-                new_subsets.insert(new_subset);
-            }
-        }
+fn bron_kerbosch(
+    graph: &FxHashMap<String, Vec<String>>,
+    r: &mut Vec<String>,
+    p: &Vec<String>,
+    x: &Vec<String>,
+    cliques: &mut FxHashSet<Vec<String>>,
+) {
+    if p.is_empty() && x.is_empty() {
+        cliques.insert(r.clone());
+        return;
     }
-    new_subsets
+    let mut p = p.clone();
+    while !p.is_empty() {
+        let v = p.pop().unwrap();
+        let mut r = r.clone();
+        r.push(v.clone());
+        let mut p = p.clone();
+        let mut x = x.clone();
+        p.retain(|x| graph.get(&v).unwrap().contains(x));
+        x.retain(|x| graph.get(&v).unwrap().contains(x));
+        bron_kerbosch(graph, &mut r, &p, &x, cliques);
+        x.push(v);
+    }
 }
 
 pub fn solve(input: &str) -> String {
-    let mut pairs: FxHashMap<String, Vec<String>> = FxHashMap::default();
-    let mut best_subsets: FxHashSet<Vec<String>> = FxHashSet::default();
+    let mut graph: FxHashMap<String, Vec<String>> = FxHashMap::default();
     for line in input.lines() {
         // println!("{:?}", line);
         let mut parts = line.split("-");
         let a = parts.next().unwrap().to_string();
         let b = parts.next().unwrap().to_string();
-        pairs.entry(a.clone()).or_insert(Vec::new()).push(b.clone());
-        pairs.entry(b.clone()).or_insert(Vec::new()).push(a.clone());
-        for c in pairs.get(&a).unwrap() {
-            if pairs.get(c).unwrap().contains(&b) {
-                let mut new_trio = [a.clone(), b.clone(), c.clone()];
-                new_trio.sort();
-                best_subsets.insert(new_trio.to_vec());
-            }
-        }
+        graph.entry(a.clone()).or_insert(Vec::new()).push(b.clone());
+        graph.entry(b.clone()).or_insert(Vec::new()).push(a.clone());
     }
-    while best_subsets.len() > 1 {
-        // println!("{:?} best_subsets of len {}", best_subsets.len(), best_subsets.iter().next().unwrap().len());
-        best_subsets = expand_subsets(&best_subsets, &pairs);
-    }
-    // println!("{:?} best_subsets", best_subsets.len());
-    // println!("{:?} terminals", pairs.keys().len());
-    // println!("{:?} pairs", pairs.values().map(Vec::len).sum::<usize>());
-    // println!("{:?} best_subset", best_subsets);
-    best_subsets.iter().next().unwrap().join(",")
+
+    let mut cliques: FxHashSet<Vec<String>> = FxHashSet::default();
+    bron_kerbosch(
+        &graph,
+        &mut Vec::new(),
+        &graph.keys().cloned().collect(),
+        &Vec::new(),
+        &mut cliques,
+    );
+    cliques
+        .into_iter()
+        .max_by_key(|clique| clique.len())
+        .unwrap()
+        .into_iter()
+        .sorted()
+        .collect::<Vec<String>>()
+        .join(",")
 }
