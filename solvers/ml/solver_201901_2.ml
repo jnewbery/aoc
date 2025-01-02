7309720495c73f8ddeb22d4a44514ba73dfd39c8
John Newbery <john@johnnewbery.com>
2025-01-02 10:08:17 +0000

    Add ocaml solver for 201901_2

--- solvers/ml/solver_201901_2.ml
+++ solvers/ml/solver_201901_2.ml
@@ -9,10 +9,22 @@ let read_integers_from_file content =
   |> List.filter (fun s -> String.trim s <> "")  (* Remove empty strings *)
   |> List.map int_of_string  (* Convert to integers *)
 
+let repeatedly_apply_and_sum op pred x =
+  let rec aux acc current first_call =
+    if pred current then acc
+    else aux (if first_call then acc else acc + current) (op current) false
+  in
+  aux 0 x true
+
+let sum_transformed_list lst op pred =
+  lst
+  |> List.map (repeatedly_apply_and_sum op pred)
+  |> List.fold_left (+) 0
+
 let solve content =
   let start_time = Unix.gettimeofday () in
   let integers = read_integers_from_file content in
-  let fuel = List.fold_left (fun acc mass -> acc + (mass / 3) - 2) 0 integers in
+  let fuel = sum_transformed_list integers (fun x -> x / 3 - 2) ((>=) 0) in
   let end_time = Unix.gettimeofday () in
   let execution_time = (end_time -. start_time) *. 1_000_000.0 in (* Convert to microseconds *)
   Printf.printf "{\"solution\": \"%d\", \"execution_time\": %.0f}\n" fuel execution_time
