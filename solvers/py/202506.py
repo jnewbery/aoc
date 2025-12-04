65b60bba9f65bd98c9ce9ba03f137d6f896ff050
John Newbery <john@johnnewbery.com>
2025-12-04 07:46:47 +0000

    Add skeleton py solvers for 2025

---
+++ solvers/py/202506.py
@@ -0,0 +1,11 @@
+from utils import exit_not_implemented
+
+def part1(ll: list[str]) -> str:
+    exit_not_implemented()
+    del ll
+    return ""
+
+def part2(ll: list[str]) -> str:
+    exit_not_implemented()
+    del ll
+    return ""
