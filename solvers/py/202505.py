55f909eaf49c697b9fa32d2dcf307c5972085481
John Newbery <john@johnnewbery.com>
2025-12-05 20:04:27 +0000

    Remove debug prints

--- solvers/py/202505.py
+++ solvers/py/202505.py
@@ -4,11 +4,9 @@ def part1(ll: list[str]) -> str:
         start, end = [int(n) for n in l.split('-')]
         fresh_ranges.append((start, end))
     fresh_ranges.sort()
-    # print(fresh_ranges)
 
     ingredients = [int(l) for l in ll]
     ingredients.sort()
-    # print(ingredients)
 
     fresh: set[int] = set()
     for ing in ingredients:
@@ -19,7 +17,6 @@ def part1(ll: list[str]) -> str:
                 continue
             else:
                 fresh.add(ing)
-    # print(fresh)
 
     return str(len(fresh))
 
@@ -29,20 +26,15 @@ def part2(ll: list[str]) -> str:
         start, end = [int(n) for n in l.split('-')]
         fresh_ranges.append((start, end))
     fresh_ranges.sort()
-    # print(fresh_ranges)
-    # print(len(fresh_ranges))
 
-    # Merge overlapping
+    # Merge overlapping ranges
     i = 0
     while i < len(fresh_ranges) - 1:
-        # print(i, fresh_ranges[i], fresh_ranges[i + 1])
         if fresh_ranges[i][1] >= fresh_ranges[i + 1][0] - 1:
-            # merge
+            # merge ranges
             fresh_ranges[i] = (fresh_ranges[i][0], max(fresh_ranges[i][1], fresh_ranges[i + 1][1]))
             fresh_ranges.pop(i + 1)
         else:
             i += 1
-    # print(fresh_ranges)
-    # print(len(fresh_ranges))
 
     return str(sum([fr[1] - fr[0] + 1 for fr in fresh_ranges]))
