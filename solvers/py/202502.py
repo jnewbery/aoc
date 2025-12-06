dcd2aeab89010f469467d61acc163dadf15a435c
John Newbery <john@johnnewbery.com>
2025-12-06 10:59:01 +0000

    Reimplement 2025021

--- solvers/py/202502.py
+++ solvers/py/202502.py
@@ -5,9 +5,7 @@ def merge_and_split(pairs: list[tuple[int, int]]) -> list[tuple[int, int]]:
     - merge any overlapping or adjacent ranges so that no range overlaps
     - split ranges by number of digits, eg [[97, 105]] becomes [[97, 99], [100, 105]]
     """
-    print(pairs, len(pairs))
     pairs.sort()
-    print(pairs, len(pairs))
 
     i = 0
     while i < len(pairs) - 1:
@@ -29,42 +27,37 @@ def merge_and_split(pairs: list[tuple[int, int]]) -> list[tuple[int, int]]:
         else:
             i += 1
 
-    print(pairs, len(pairs))
     return pairs
 
 
-def rep_digit_block_nums_below(x: int) -> set[int]:
+def sum_rep_2_digit_block_nums_between(lower: int, higher: int) -> int:
     """
-    An RDB (repeated-digit-block) number is of the form 'abcabc', eg 183183.
+    An R2DB (repeated-2-digit-block) number is of the form 'abcabc', eg 183183.
     If the block is n digits, then the RDB number is x * (10^n + 1).
 
     x * (10^n + 1) is a RDB number for all 10^(n-1) <= x <= 10^n - 1.
 
     For example, if n is 2, then 1010, 1111, 1212, ..., 9898, 9999 are all RDB numbers.
 
-    This function returns a set of all of the RDB numbers below a given value.
+    This function returns a sum of all of the RDB numbers below a given value, with a length equal to the given value.
     """
-    if x < 10:
-        return set()
+    # assert len(str(lower)) == len(str(higher)), "Solver only implemented for pairs of numbers of length differing by at most one"
+    digits = int(math.log10(lower)) + 1
 
-    digits = int(math.log10(x)) + 1
+    if digits % 2 != 0:
+        return 0
 
-    rdbns: set[int] = set()
-    # Find the number of RDB numbers of digit length digits, less than x
-    if digits % 2 == 0:
-        n = digits / 2
-        top_rdbn = math.floor(x / (10 ** n + 1))
-        rdbns.update({i * int(10 ** n + 1) for i in range(int(10 ** (n - 1)), top_rdbn + 1)})
-        n -= 1
-    else:
-        n = (digits - 1) / 2
-
-    rdbns.update({i * int(10 ** n + 1) for i in range(int(10 ** (n - 1)), int(10 ** n))})
-
-    return rdbns
+    # Find the RDB numbers of digit length digits, less than higher but more than lower
+    n = digits / 2
+    top_rdbn = math.floor(higher / (10 ** n + 1))
+    bottom_rdbn = math.ceil(lower / (10 ** n + 1))
+    rdbs = [i * int(10 ** n + 1) for i in range(bottom_rdbn, top_rdbn + 1)]
+    # print(f"{rdbs=}")
+    # print(f"{sum(rdbs)=}")
+    return sum(rdbs)
 
 def part1(ll: list[str]) -> str:
-    invalids: set[int] = set()
+    invalids: int = 0
     pairs: list[tuple[int, int]] = []
     for str_pair in ll[0].split(','):
         pair = str_pair.split('-')
@@ -72,9 +65,9 @@ def part1(ll: list[str]) -> str:
     pairs = merge_and_split(pairs)
 
     for pair in pairs:
-        new_invalids = rep_digit_block_nums_below(pair[1]) - rep_digit_block_nums_below(pair[0] - 1)
-        invalids ^= new_invalids
-    return str(sum(invalids))
+        new_invalids = sum_rep_2_digit_block_nums_between(*pair)
+        invalids += new_invalids
+    return str(invalids)
 
 def part2(ll: list[str]) -> str:
     for pair in ll[0].split(','):
