b19bccbb572e69c01da6de417abdeb9feb83137a
John Newbery <john@johnnewbery.com>
2025-12-06 17:54:22 +0000

    Clean up 202501

--- solvers/py/202501.py
+++ solvers/py/202501.py
@@ -1,27 +1,36 @@
+DIAL_SIZE = 100
+
 def part1(ll: list[str]) -> str:
     pos = 50
-    ret = 0
+    count = 0
     for l in ll:
-        if l[0] == 'R':
-            pos = (pos + int(l[1:])) % 100
+        direction, steps = l[0], int(l[1:])
+        if direction == 'R':
+            pos = (pos + steps) % DIAL_SIZE
         else:
-            pos = (pos - int(l[1:])) % 100
+            pos = (pos - steps) % DIAL_SIZE
         if pos == 0:
-            ret += 1
-    return str(ret)
+            count += 1
+    return str(count)
 
 def part2(ll: list[str]) -> str:
     pos = 50
-    ret = 0
+    count = 0
     for l in ll:
-        if l[0] == 'R':
-            rots, pos = divmod(pos + int(l[1:]), 100)
-            ret += rots
+        direction, steps = l[0], int(l[1:])
+        if direction == 'R':
+            # rots is how many rotations we made past 0
+            rots, pos = divmod(pos + steps, DIAL_SIZE)
+            count += rots
         else:
             if pos == 0:
-                ret -= 1
-            rots, pos = divmod(pos - int(l[1:]), 100)
-            ret += abs(rots)
+                # If we stopped on 0 last time, we need to make sure we don't
+                # double count when we go backwards
+                count -= 1
+            rots, pos = divmod(pos - steps, DIAL_SIZE)
+            count += abs(rots)
             if pos == 0:
-                ret += 1
-    return str(ret)
+                # If we land on 0, we need to count that as passing it (even
+                # though rots hasn't reduced)
+                count += 1
+    return str(count)
