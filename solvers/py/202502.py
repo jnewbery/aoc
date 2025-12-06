5ac3f4b28be5e3053852465a2375960c98c4070d
John Newbery <john@johnnewbery.com>
2025-12-06 10:38:34 +0000

    202502: implement merge and split

--- solvers/py/202502.py
+++ solvers/py/202502.py
@@ -1,5 +1,38 @@
 import math
 
+def merge_and_split(pairs: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    """Given a list of ranges:
+    - merge any overlapping or adjacent ranges so that no range overlaps
+    - split ranges by number of digits, eg [[97, 105]] becomes [[97, 99], [100, 105]]
+    """
+    print(pairs, len(pairs))
+    pairs.sort()
+    print(pairs, len(pairs))
+
+    i = 0
+    while i < len(pairs) - 1:
+        if pairs[i][1] >= pairs[i + 1][0] - 1:
+            # merge ranges
+            pairs[i] = (pairs[i][0], max(pairs[i][1], pairs[i + 1][1]))
+            pairs.pop(i + 1)
+        else:
+            i += 1
+
+    i = 0
+    while i < len(pairs) - 1:
+        pair = pairs[i]
+        if len(str(pair[0])) != len(str(pair[1])):
+            pairs.pop(i)
+            pairs.insert(i, (10 ** len(str(pair[0])), pair[1]))
+            pairs.insert(i, (pair[0], 10 ** len(str(pair[0])) - 1))
+            i += 2
+        else:
+            i += 1
+
+    print(pairs, len(pairs))
+    return pairs
+
+
 def rep_digit_block_nums_below(x: int) -> set[int]:
     """
     An RDB (repeated-digit-block) number is of the form 'abcabc', eg 183183.
@@ -32,10 +65,14 @@ def rep_digit_block_nums_below(x: int) -> set[int]:
 
 def part1(ll: list[str]) -> str:
     invalids: set[int] = set()
-    for pair in ll[0].split(','):
-        lower, higher = pair.split('-')
-        assert abs(len(lower) - len(higher)) <= 1, "Solver only implemented for pairs of numbers of length differing by at most one"
-        new_invalids = rep_digit_block_nums_below(int(higher)) - rep_digit_block_nums_below(int(lower) - 1)
+    pairs: list[tuple[int, int]] = []
+    for str_pair in ll[0].split(','):
+        pair = str_pair.split('-')
+        pairs.append((int(pair[0]), int(pair[1])))
+    pairs = merge_and_split(pairs)
+
+    for pair in pairs:
+        new_invalids = rep_digit_block_nums_below(pair[1]) - rep_digit_block_nums_below(pair[0] - 1)
         invalids ^= new_invalids
     return str(sum(invalids))
 
