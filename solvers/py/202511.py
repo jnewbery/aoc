bebc996de6a53747ee1e832e4a7cb14a01c6f3e5
John Newbery <john@johnnewbery.com>
2025-12-15 16:25:08 +0000

    Improve 202511

--- solvers/py/202511.py
+++ solvers/py/202511.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from collections import defaultdict
+from itertools import pairwise
 
 @dataclass
 class Device():
     name: str
     to_devices: list[str]
     depth: None | int  # greatest depth (longest path from start)
-    paths: int # number of different paths to reach this device
 
 def get_devices(ll: list[str], start_name: str, end_name: str) -> dict[str, Device]:
     devices: dict[str, Device] = {}
@@ -17,38 +18,42 @@ def get_devices(ll: list[str], start_name: str, end_name: str) -> dict[str, Devi
             continue
         elif from_device == start_name:
             depth = 0
-            paths = 1
         else:
             depth = None
-            paths = 0
-        devices[from_device] = Device(name=from_device, to_devices=to_devices, depth=depth, paths=paths)
-    devices[end_name] = Device(name=end_name, to_devices=[], depth=None, paths=0)
+        devices[from_device] = Device(name=from_device, to_devices=to_devices, depth=depth)
+    devices[end_name] = Device(name=end_name, to_devices=[], depth=None)
     return devices
 
-def get_depths(devices: dict[str, Device], start_name: str, end_name: str) -> list[str]:
+def get_depths(devices: dict[str, Device], start_name: str) -> list[str]:
+    depths: dict[str, int] = {start_name: 0}
     to_visit: list[str] = [start_name]
     while to_visit:
         visiting = devices[to_visit.pop()]
-        assert visiting.depth is not None
+        assert visiting.name in depths
         for to_visit_str in visiting.to_devices:
-            to_visit_device = devices[to_visit_str]
-
-            if to_visit_device.depth == None or to_visit_device.depth < visiting.depth + 1:
-                to_visit_device.depth = visiting.depth + 1
+            if to_visit_str not in depths or depths[to_visit_str] < depths[visiting.name] + 1:
+                depths[to_visit_str] = depths[visiting.name] + 1
                 to_visit.append(to_visit_str)
 
-    devices_by_depth: list[Device] = [d for d in devices.values() if d.depth is not None]
-    devices_by_depth.sort(key=lambda d: d.depth or 0)
+    return sorted(depths.keys(), key=lambda n: depths[n])
 
-    return [d.name for d in devices_by_depth] + [end_name]
+def get_paths(devices: dict[str, Device], devices_by_depth: list[str], start_name: str, end_name: str) -> int:
+    assert start_name in devices_by_depth
+    assert end_name in devices_by_depth
+    assert devices_by_depth.index(start_name) < devices_by_depth.index(end_name)
 
-def get_paths(devices: dict[str, Device], devices_by_depth: list[str], end_name: str):
-    for visiting_name in devices_by_depth:
+    paths: defaultdict[str, int] = defaultdict(int)
+    paths[start_name] = 1
+    # breakpoint()
+    for visiting_name in devices_by_depth[devices_by_depth.index(start_name):]:
         visiting = devices[visiting_name]
         if visiting_name == end_name:
-            return
+            return paths[end_name]
         for to_visit_str in visiting.to_devices:
-            devices[to_visit_str].paths += visiting.paths
+            paths[to_visit_str] += paths[visiting_name]
+
+    # Shouldn't reach this point - we should always pass end_name as we iterate over devices_by_depth
+    assert False
 
 def part1(ll: list[str]) -> str:
     START_NAME = "you"
@@ -56,12 +61,12 @@ def part1(ll: list[str]) -> str:
     devices: dict[str, Device] = get_devices(ll, START_NAME, END_NAME)
 
     # calculate depths
-    devices_by_depth = get_depths(devices, START_NAME, END_NAME)
+    devices_by_depth = get_depths(devices, START_NAME)
 
     # BFS by depth
-    get_paths(devices, devices_by_depth, END_NAME)
+    paths = get_paths(devices, devices_by_depth, START_NAME, END_NAME)
 
-    return str(devices[END_NAME].paths)
+    return str(paths)
 
 def part2(ll: list[str]) -> str:
     START_NAME = "svr"
@@ -71,17 +76,13 @@ def part2(ll: list[str]) -> str:
     devices: dict[str, Device] = get_devices(ll, START_NAME, END_NAME)
 
     # calculate depths
-    devices_by_depth = get_depths(devices, START_NAME, END_NAME)
+    devices_by_depth = get_depths(devices, START_NAME)
 
-    sorted_waypoints = sorted(WAYPOINTS[:], key=lambda w: devices[w].depth or 0) + [END_NAME]
+    sorted_waypoints = [START_NAME] + sorted(WAYPOINTS[:], key=lambda w: devices_by_depth.index(w) or 0) + [END_NAME]
 
     # BFS by depth to each waypoint in turn
-    for waypoint in sorted_waypoints:
-        get_paths(devices, devices_by_depth, waypoint)
-        for d in devices.values():
-            if d.name != waypoint:
-                # Clear out the number of paths to each node, except for the
-                # just-calculated paths to the waypoint.
-                d.paths = 0
+    paths = 1
+    for start, end in pairwise(sorted_waypoints):
+        paths *= get_paths(devices, devices_by_depth, start, end)
 
-    return str(devices[END_NAME].paths)
+    return str(paths)
