e326f17016694bcc2d5b1d20d3a8f1ab67c2cda4
John Newbery <john@johnnewbery.com>
2025-12-23 11:32:24 +0000

    Complete 202217

--- solvers/py/202217.py
+++ solvers/py/202217.py
@@ -1,5 +1,4 @@
 from itertools import cycle, count
-from utils import exit_not_implemented
 
 _ROCKS_INPUT = [
    "####",
@@ -20,6 +19,66 @@ _ROCKS_INPUT = [
     "##\n"
     "##",
 ]
+
+def _solve(ll: list[str], num_rocks: int) -> str:
+    max_height = 0
+    stopped_rocks: set[tuple[int, int]] = set()
+    winds: list[tuple[int, int]] = [(1, 0) if c == ">" else (-1, 0) for c in ll[0]]
+    rocks = _get_rocks()
+
+    # Cache of (rock_index, wind_index) -> (rock_count, max_height, last_cycle_len)
+    cache: dict[tuple[int, int], tuple[int, int, int | None]] = {}
+    rock_to_height: dict[int, int] = {}
+
+    j = 0
+    for i in count(1):
+        rock = rocks[(i - 1) % len(rocks)]
+        if i == num_rocks + 1:
+            return str(max_height)
+        rock = move_rock(rock, (2, max_height + 4))
+
+        while True:
+            # Move rock according to wind direction
+            next_wind = winds[j % len(winds)]
+            # print(f"Rock {i % len(rocks)}, wind {j % len(winds)}")
+            j += 1
+            next_rock_pos = move_rock(rock, next_wind)
+            if any((cell[0] < 0 or cell[0] >= 7 for cell in next_rock_pos)):
+                pass
+            elif any((cell in stopped_rocks for cell in next_rock_pos)):
+                pass
+            else:
+                rock = next_rock_pos
+            # _print_tunnel(stopped_rocks, rock)
+
+            # Move rock down
+            next_rock_pos = move_rock(rock, (0, -1))
+            if any((cell in stopped_rocks for cell in next_rock_pos)) or any((y == 1 for _, y in rock)):
+                break
+            else:
+                rock = next_rock_pos
+            # _print_tunnel(stopped_rocks, rock)
+
+        stopped_rocks |= rock
+        max_height = max(max_height, max((y for _, y in rock)))
+
+        rock_to_height[i] = max_height
+        cache_key = (i % len(rocks), j % len(winds))
+        if (cache_entry := cache.get(cache_key, None)) is not None:
+            rock_diff = i - cache_entry[0]
+            height_diff = max_height - cache_entry[1]
+            # print(f"Cycle found! {cache_key} -> {cache_entry}, {rock_diff=}, {height_diff=}")
+            if cache_entry[2] == rock_diff:
+                rocks_left = num_rocks - i
+                cycles, remainder = divmod(rocks_left, rock_diff)
+                height = max_height + cycles * height_diff + rock_to_height[cache_entry[0] + remainder] - rock_to_height[cache_entry[0]]
+                return str(height)
+            cache[cache_key] = (i, max_height, rock_diff)
+        else:
+            cache[cache_key] = (i, max_height, None)
+
+    assert False, "Should always terminate"
+
 
 def _get_rocks() -> list[set[tuple[int, int]]]:
     rocks: list[set[tuple[int, int]]] = []
@@ -73,8 +132,8 @@ def part1(ll: list[str], args=None) -> str:
         rock = move_rock(rock, (2, max_height + 4))
         while True:
             # Move rock according to wind direction
-            j, next_wind = next(winds)
-            print(f"Rock {i}, wind {j}")
+            _, next_wind = next(winds)
+            # print(f"Rock {i}, wind {j}")
             next_rock_pos = move_rock(rock, next_wind)
             if any((cell[0] < 0 or cell[0] >= 7 for cell in next_rock_pos)):
                 pass
@@ -101,54 +160,4 @@ def part1(ll: list[str], args=None) -> str:
 
 def part2(ll: list[str], args=None) -> str:
     del args
-    exit_not_implemented()
-    # TODO:
-    # - Detect cycle of (rock_index, wind_index)
-    # - Calculate cycle length and change in max_height over cycle
-    # - Calculate what max_height would be after rock 1_000_000_000_000
-    NUM_ROCKS = 1_000_000_000_000
-    last_max_height = 0
-    max_height = 0
-    stopped_rocks: set[tuple[int, int]] = set()
-    winds: list[tuple[int, int]] = [(1, 0) if c == ">" else (-1, 0) for c in ll[0]]
-    rocks = _get_rocks()
-
-    j = 0
-    for i in count():
-        rock = rocks[i % len(rocks)]
-        if i == NUM_ROCKS:
-            break
-        rock = move_rock(rock, (2, max_height + 4))
-        if i % len(rocks) == 0:
-            print(f"Rock {i % len(rocks)}, wind {j % len(winds)}")
-        if j % len(winds) == 0:
-            print(f"Rock {i}, wind {j % len(winds)}, max height {max_height}, delta {max_height - last_max_height}")
-            last_max_height = max_height
-        while True:
-            # Move rock according to wind direction
-            next_wind = winds[j % len(winds)]
-            # print(f"Rock {i % len(rocks)}, wind {j % len(winds)}")
-            j += 1
-            next_rock_pos = move_rock(rock, next_wind)
-            if any((cell[0] < 0 or cell[0] >= 7 for cell in next_rock_pos)):
-                pass
-            elif any((cell in stopped_rocks for cell in next_rock_pos)):
-                pass
-            else:
-                rock = next_rock_pos
-            # _print_tunnel(stopped_rocks, rock)
-
-            # Move rock down
-            next_rock_pos = move_rock(rock, (0, -1))
-            if any((cell in stopped_rocks for cell in next_rock_pos)) or any((y == 1 for _, y in rock)):
-                stopped_rocks |= rock
-                max_height = max(max_height, max((y for _, y in rock)))
-                break
-            else:
-                rock = next_rock_pos
-            # _print_tunnel(stopped_rocks, rock)
-
-        # print(rock, max_height)
-        # _print_tunnel(stopped_rocks, rock)
-
-    return str(max_height)
+    return _solve(ll, 1_000_000_000_000)
