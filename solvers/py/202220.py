4367ce82c83a161c2213266d285d8fd7df630d2e
John Newbery <john@johnnewbery.com>
2025-12-27 17:17:21 +0000

    Try to fix 2022201. It's still broken
    The full input contains duplicates, so you need to keep track of the
    original positions of each element.

--- solvers/py/202220.py
+++ solvers/py/202220.py
@@ -1,35 +1,40 @@
 from utils import exit_not_implemented
+from collections import deque
 
 def part1(ll: list[str], args=None) -> str:
     del args
 
-    original_list = [int(x) for x in ll]
-    list_len = len(original_list)
+    d: deque[tuple[int, int]] = deque([(n, int(x)) for n,x in enumerate(ll)])
+    list_len = len(d)
 
-    new_list = original_list.copy()
-    # print(new_list)
+    print(d)
 
-    for i in original_list:
-        i_index = new_list.index(i)
-        new_list.pop(i_index)
-        new_index = i_index + i
-        if new_index <= 0:
-            new_index += (list_len - 1)
-        if new_index >= list_len:
-            new_index -= (list_len - 1)
-        # print(f"Moving {i} from {i_index} to {new_index}")
-        new_list.insert(new_index, i)
-        # print(new_list)
+    for i in range(len(d)):
+        for current_pos, p in enumerate(d):
+            if p[0] == i:
+                print(f"Moving {p[1]}")
 
-    print(new_list)
+                # rotate so that i is at the front
+                d.rotate(-current_pos)
+                # remove the element
+                d.popleft()
+                # rotate the deque
+                d.rotate(-(i + 1))
+                d.appendleft(p)
+                print([p[1] for p in d])
+                break
 
-    zero_index = new_list.index(0)
-    i_1000 = new_list[(zero_index + 1000) % list_len]
-    print(f"{i_1000=}")
-    i_2000 = new_list[(zero_index + 2000) % list_len]
-    print(f"{i_2000=}")
-    i_3000 = new_list[(zero_index + 3000) % list_len]
-    print(f"{i_3000=}")
+    # print(d)
+
+    zero_index = 0
+    for n, x in enumerate(d):
+        if x == 0:
+            zero_index = n
+    # breakpoint()
+    i_1000 = d[(zero_index + 1000) % list_len][1]
+    i_2000 = d[(zero_index + 2000) % list_len][1]
+    i_3000 = d[(zero_index + 3000) % list_len][1]
+    print(f"{i_1000=}, {i_2000=}, {i_3000=}")
     sol = i_1000 + i_2000 + i_3000
     return str(sol)
 
